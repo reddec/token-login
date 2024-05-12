365dcfad61c681ab16fc696fd9a8eb33d1b4b1ff
Aleksandr Baryshnikov <owner@reddec.net>
2024-05-12 18:49:25 +0800

    make validator more abstract

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,83 +1,134 @@
 package validator
 
 import (
-	"context"
+	"crypto/subtle"
 	"errors"
 	"fmt"
+	"sync"
 	"time"
 
+	"github.com/gobwas/glob"
 	"github.com/reddec/token-login/internal/types"
-
-	"github.com/reddec/token-login/internal/dbo"
-	"github.com/reddec/token-login/internal/utils"
+	"golang.org/x/crypto/sha3"
 )
 
 var ErrInvalidToken = errors.New("invalid token")
 
-type Storage interface {
-	FindToken(ctx context.Context, id types.KeyID) (*types.Token, error)
-	UpdateTokensStats(ctx context.Context, stats []dbo.TokenStat) error
+type State map[types.KeyID]*AccessKey
+
+type Hit struct {
+	Time time.Time
+	ID   int
 }
 
 type Validator struct {
-	stats utils.Stats
-	cache *cachedStorage
-	store Storage
+	state struct {
+		data State
+		lock sync.RWMutex
+	}
+	accessLog chan Hit
 }
 
-func NewValidator(storage Storage, cacheCapacity int, cacheTTL time.Duration) *Validator {
+func NewValidator(statsBuffer int) *Validator {
 	v := &Validator{
-		store: storage,
-		cache: newCachedStorage(storage, cacheTTL, cacheCapacity),
+		accessLog: make(chan Hit, statsBuffer),
 	}
 	return v
 }
 
-func (v *Validator) Invalidate(keyID types.KeyID) {
-	v.cache.Clear(keyID)
+func (v *Validator) Set(state State) {
+	v.state.lock.Lock()
+	defer v.state.lock.Unlock()
+	v.state.data = state
 }
 
-func (v *Validator) Valid(ctx context.Context, host, path string, token string) (*types.Token, error) {
-	t, err := v.validate(ctx, host, path, token)
-	if err != nil {
-		return t, err
-	}
-	v.stats.Inc(int64(t.ID))
-	return t, nil
+func (v *Validator) AccessLog() <-chan Hit {
+	return v.accessLog
 }
 
-func (v *Validator) validate(ctx context.Context, host, path string, token string) (*types.Token, error) {
-	// parse token
+func (v *Validator) Validate(host, path string, token string) (*AccessKey, error) {
 	key, err := types.ParseToken(token)
 	if err != nil {
 		return nil, fmt.Errorf("parse key: %w", err)
 	}
 
-	dbToken, err := v.cache.Get(ctx, key.ID())
-	if err != nil {
-		return nil, fmt.Errorf("get token: %w", err)
+	kid := key.ID()
+
+	accessKey, ok := v.findAccessKey(kid)
+	if !ok {
+		return nil, fmt.Errorf("find key %s: %w", kid.String(), ErrInvalidToken)
 	}
 
-	if !dbToken.Valid(host, path, key.Payload()) {
-		return nil, ErrInvalidToken
+	if !accessKey.Valid(host, path, key.Payload()) {
+		return nil, fmt.Errorf("validate %s: %w", kid.String(), ErrInvalidToken)
 	}
-	return dbToken, nil
+
+	select {
+	case v.accessLog <- Hit{Time: time.Now(), ID: accessKey.ID()}:
+	default:
+	}
+
+	return accessKey, nil
 }
 
-func (v *Validator) UpdateStats(ctx context.Context) error {
-	stats := v.stats.Pop()
-	if len(stats) == 0 {
-		return nil
+func (v *Validator) findAccessKey(id types.KeyID) (*AccessKey, bool) {
+	v.state.lock.RLock()
+	defer v.state.lock.RUnlock()
+	t, ok := v.state.data[id]
+	return t, ok
+}
+
+func NewAccessKey(id int, hash []byte, host, path string, headers types.Headers) (*AccessKey, error) {
+	if host == "" {
+		host = "**"
+	}
+	hostGlob, err := glob.Compile(host, '.')
+	if err != nil {
+		return nil, fmt.Errorf("compile host glob: %w", err)
 	}
 
-	var storeStats = make([]dbo.TokenStat, 0, len(stats))
-	for _, s := range stats {
-		storeStats = append(storeStats, dbo.TokenStat{
-			Token: int(s.ID),
-			Last:  time.Unix(0, s.Last),
-			Hits:  s.Requests,
-		})
+	if path == "" {
+		path = "/**"
+	}
+	pathGlob, err := glob.Compile(path, '/')
+	if err != nil {
+		return nil, fmt.Errorf("compile path glob: %w", err)
 	}
 
-	return v.store.UpdateTokensStats(ctx, storeStats)
+	return &AccessKey{
+		id:       id,
+		hash:     hash,
+		headers:  headers,
+		pathGlob: pathGlob,
+		hostGlob: hostGlob,
+	}, nil
+}
+
+type AccessKey struct {
+	id       int
+	hash     []byte
+	headers  types.Headers
+	pathGlob glob.Glob
+	hostGlob glob.Glob
+}
+
+func (t *AccessKey) Valid(host, path string, payload []byte) bool {
+	if !t.hostGlob.Match(host) {
+		return false
+	}
+
+	if !t.pathGlob.Match(path) {
+		return false
+	}
+
+	hash := sha3.Sum384(payload)
+	return subtle.ConstantTimeCompare(hash[:], t.hash) == 1
+}
+
+func (t *AccessKey) Headers() types.Headers {
+	return t.headers
+}
+
+func (t *AccessKey) ID() int {
+	return t.id
 }
--- internal/validator/validator_test.go
+++ internal/validator/validator_test.go
@@ -1,133 +1,141 @@
 package validator_test
 
 import (
-	"context"
+	"sync/atomic"
 	"testing"
-	"time"
 
 	"github.com/reddec/token-login/internal/types"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/reddec/token-login/internal/dbo"
 	"github.com/reddec/token-login/internal/validator"
 )
 
 func TestValidator_Valid(t *testing.T) {
-	ctx := context.Background()
-	store, err := dbo.New(ctx, "file::memory:?cache=shared", nil)
-	require.NoError(t, err)
+	v := validator.NewValidator(1024)
 
-	v := validator.NewValidator(store, 10, 1*time.Minute)
+	state := make(validator.State)
 
-	key, err := types.NewKey()
-	require.NoError(t, err)
-	require.NoError(t, store.CreateToken(ctx, dbo.TokenParams{
-		User: "admin",
-		Config: dbo.TokenConfig{
-			Label: "demo",
-			Path:  "/**",
-		},
-		Key: key,
-	}))
+	demo := genKey("", "/**").Into(state)
+	demo2 := genKey("", "/hello").Into(state)
+	demo3 := genKey("example.com", "/**").Into(state)
+	demo4 := genKey("*.example.com", "/**").Into(state)
 
-	key2, err := types.NewKey()
-	require.NoError(t, err)
-	require.NoError(t, store.CreateToken(ctx, dbo.TokenParams{
-		User: "user",
-		Config: dbo.TokenConfig{
-			Label: "demo 2",
-			Path:  "/hello",
-		},
-		Key: key2,
-	}))
-
-	key3, err := types.NewKey()
-	require.NoError(t, err)
-	require.NoError(t, store.CreateToken(ctx, dbo.TokenParams{
-		User: "user",
-		Config: dbo.TokenConfig{
-			Label: "demo 3",
-			Path:  "/**",
-			Host:  "example.com",
-		},
-		Key: key3,
-	}))
-
-	key4, err := types.NewKey()
-	require.NoError(t, err)
-	require.NoError(t, store.CreateToken(ctx, dbo.TokenParams{
-		User: "user",
-		Config: dbo.TokenConfig{
-			Label: "demo 4",
-			Path:  "/**",
-			Host:  "*.example.com",
-		},
-		Key: key4,
-	}))
-
-	t.Logf("KeyID 1: %s\nKeyID 2: %s", key.ID().String(), key2.ID().String())
+	v.Set(state)
 
 	t.Run("basic test is ok", func(t *testing.T) {
-		found, err := v.Valid(ctx, "", "/", key.String())
+		found, err := v.Validate("", "/", demo.String())
 		require.NoError(t, err)
-		assert.Equal(t, 1, found.ID)
+		assert.Equal(t, 1, found.ID())
 
-		found2, err := v.Valid(ctx, "", "/hello", key2.String())
+		found2, err := v.Validate("", "/hello", demo2.String())
 		require.NoError(t, err)
-		assert.Equal(t, 2, found2.ID)
+		assert.Equal(t, 2, found2.ID())
 	})
 
 	t.Run("path validation for glob", func(t *testing.T) {
-		found, err := v.Valid(ctx, "", "/something", key.String())
+		found, err := v.Validate("", "/something", demo.String())
 		require.NoError(t, err)
-		assert.Equal(t, 1, found.ID)
+		assert.Equal(t, 1, found.ID())
 	})
 
 	t.Run("path validation restricted", func(t *testing.T) {
-		_, err := v.Valid(ctx, "", "/something", key2.String())
+		_, err := v.Validate("", "/something", demo2.String())
 		require.Error(t, err)
 	})
 
 	t.Run("dump stats", func(t *testing.T) {
-		s, err := store.FindToken(ctx, key.ID())
-		require.NoError(t, err)
-		assert.Empty(t, s.Requests)
+		_ = readStats(v.AccessLog()) // clear
 
-		_, err = v.Valid(ctx, "", "/something", key.String())
+		_, err := v.Validate("", "/something", demo.String())
 		require.NoError(t, err)
 
-		err = v.UpdateStats(ctx)
-		require.NoError(t, err)
+		const repeats = 2
+		for i := 0; i < repeats; i++ {
+			_, err = v.Validate("", "/hello", demo2.String())
+			require.NoError(t, err)
+		}
 
-		s, err = store.FindToken(ctx, key.ID())
-		require.NoError(t, err)
-		assert.NotEmpty(t, s.Requests)
+		stats := readStats(v.AccessLog())
+
+		require.Len(t, stats, 2)
+		assert.Equal(t, 1, stats[demo.AccessKey.ID()])
+		assert.Equal(t, repeats, stats[demo2.AccessKey.ID()])
 	})
 
 	t.Run("valid host is working", func(t *testing.T) {
-		_, err := v.Valid(ctx, "example.com", "/something", key3.String())
+		_, err := v.Validate("example.com", "/something", demo3.String())
 		require.NoError(t, err)
 	})
 
 	t.Run("invalid host is not working", func(t *testing.T) {
-		_, err := v.Valid(ctx, "", "/something", key3.String())
+		_, err := v.Validate("", "/something", demo3.String())
 		require.Error(t, err)
 	})
 
 	t.Run("valid wildcard host is working", func(t *testing.T) {
-		_, err := v.Valid(ctx, "some.example.com", "/something", key4.String())
+		_, err := v.Validate("some.example.com", "/something", demo4.String())
 		require.NoError(t, err)
 	})
 
 	t.Run("multi-level wildcard host is not working", func(t *testing.T) {
-		_, err := v.Valid(ctx, "another.some.example.com", "/something", key4.String())
+		_, err := v.Validate("another.some.example.com", "/something", demo4.String())
 		require.Error(t, err)
 	})
 
 	t.Run("wildcard does not support root level", func(t *testing.T) {
-		_, err := v.Valid(ctx, "example.com", "/something", key4.String())
+		_, err := v.Validate("example.com", "/something", demo4.String())
 		require.Error(t, err)
 	})
+}
+
+func mustAccessKey(id int, hash []byte, host, path string, headers types.Headers) *validator.AccessKey {
+	v, err := validator.NewAccessKey(id, hash, host, path, headers)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func genKey(host, path string, headers ...types.Header) *testKey {
+	raw, err := types.NewKey()
+	if err != nil {
+		panic(err)
+	}
+	id := idSeq.Add(1)
+	accessKey := mustAccessKey(int(id), raw.Hash(), host, path, headers)
+
+	return &testKey{
+		Secret:    raw,
+		AccessKey: accessKey,
+	}
+}
+
+type testKey struct {
+	Secret    types.Key
+	AccessKey *validator.AccessKey
+}
+
+func (tk *testKey) String() string {
+	return tk.Secret.String()
+}
+
+func (tk *testKey) Into(state validator.State) *testKey {
+	state[tk.Secret.ID()] = tk.AccessKey
+	return tk
+}
+
+var idSeq atomic.Int64
+
+func readStats(content <-chan validator.Hit) map[int]int {
+	ans := make(map[int]int)
+	for {
+		select {
+		case hit := <-content:
+			ans[hit.ID]++
+		default:
+			return ans
+		}
+	}
 }
